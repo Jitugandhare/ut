a9c50a00ed5d1185a37657754bcf187ec4419bc4
Jitu gandhare <jitugandhare@gmail.com>
2024-07-17 17:47:56 +0530

    in progress

--- ut-assignment/src/components/shared/Layout.jsx
+++ ut-assignment/src/components/shared/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout() {
 		<div className="bg-neutral-100 h-screen w-screen overflow-hidden flex flex-row">
 			<Sidebar />
 			<div className="flex flex-col flex-1">
-				{/* <Header /> */}
+				<Header />
 				<div className="flex-1 p-4 min-h-0 overflow-auto">
 					<Outlet />
 				</div>
